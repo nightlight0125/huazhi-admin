29f236a12432d188b3dc4a18ff667475f502f6f7
summer <1821060237@qq.com>
2025-12-05 14:08:59 +0800

    整理菜单

--- src/features/settings/index.tsx
+++ src/features/settings/index.tsx
@@ -1,38 +1,8 @@
 import { Outlet } from '@tanstack/react-router'
-import { Bell, Monitor, Palette, UserCog, Wrench } from 'lucide-react'
 import { Separator } from '@/components/ui/separator'
 import { HeaderActions } from '@/components/header-actions'
 import { Header } from '@/components/layout/header'
 import { Main } from '@/components/layout/main'
-import { SidebarNav } from './components/sidebar-nav'
-
-const sidebarNavItems = [
-  {
-    title: 'Profile',
-    href: '/settings',
-    icon: <UserCog size={18} />,
-  },
-  {
-    title: 'Account',
-    href: '/settings/account',
-    icon: <Wrench size={18} />,
-  },
-  {
-    title: 'Appearance',
-    href: '/settings/appearance',
-    icon: <Palette size={18} />,
-  },
-  {
-    title: 'Notifications',
-    href: '/settings/notifications',
-    icon: <Bell size={18} />,
-  },
-  {
-    title: 'Display',
-    href: '/settings/display',
-    icon: <Monitor size={18} />,
-  },
-]
 
 export function Settings() {
   return (
@@ -52,9 +22,6 @@ export function Settings() {
         </div>
         <Separator className='my-4 lg:my-6' />
         <div className='flex flex-1 flex-col space-y-2 overflow-hidden md:space-y-2 lg:flex-row lg:space-y-0 lg:space-x-12'>
-          <aside className='top-0 lg:sticky lg:w-1/5'>
-            <SidebarNav items={sidebarNavItems} />
-          </aside>
           <div className='flex w-full overflow-y-hidden p-1'>
             <Outlet />
           </div>
